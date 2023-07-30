7261359e48e9503d322080d664d20b1ce5f6ae42
Николай Федосеев <xneek@mail.ru>
2023-07-31 02:24:07 +0300

    init

---
+++ src/xneek-gpx.test.ts
@@ -0,0 +1,24 @@
+import {describe, expect, test} from '@jest/globals';
+import { encodeNumbersSequence, decodeNumbersSequence} from './xneek-gpx';
+
+const samplesLat = [53.14299, 53.14238, 53.14184, 53.14128, 53.14067, 53.14003, 53.13944, 53.13885, 53.13828, 53.13767, 53.13705];
+const samplesLon = [45.04254, 45.04368, 45.04473, 45.04585, 45.04701, 45.04818, 45.04928, 45.05042, 45.05149, 45.05259, 45.05374];
+const samplesAlt = [100, 200, 300, 400, 500, 600, 500, 400, 300, 200, 100];
+
+describe('sum module', () => {
+  test('Encoded-decoded lat', () => {
+    const encoded = encodeNumbersSequence(samplesLat);
+    const decoded = decodeNumbersSequence(encoded);
+    expect(samplesLat).toStrictEqual(decoded);
+  });
+  test('Encoded-decoded lon', () => {
+    const encoded = encodeNumbersSequence(samplesLon);
+    const decoded = decodeNumbersSequence(encoded);
+    expect(samplesLon).toStrictEqual(decoded);
+  });
+  test('Encoded-decoded Alt', () => {
+    const encoded = encodeNumbersSequence(samplesAlt);
+    const decoded = decodeNumbersSequence(encoded);
+    expect(samplesAlt).toStrictEqual(decoded);
+  });
+});
---
+++ src/xneek-gpx.ts
@@ -0,0 +1,72 @@
+
+function round(value) {
+  return Math.floor(Math.abs(value) + 0.5) * (value >= 0 ? 1 : -1);
+}
+
+function encode(_current, _previous, factor) {
+  const current = round(_current * factor);
+  const previous = round(_previous * factor);
+  let coordinate = current - previous;
+  coordinate <<= 1;
+  if (current - previous < 0) {
+    coordinate = ~coordinate;
+  }
+  let output = '';
+  while (coordinate >= 0x20) {
+    output += String.fromCharCode((0x20 | (coordinate & 0x1f)) + 63);
+    coordinate >>= 5;
+  }
+  output += String.fromCharCode(coordinate + 63);
+  return output;
+}
+
+const encodeNumbersSequence = (numbersSequence: number[]) => {
+const precision = 5;
+  const factor = 10 ** precision;
+  let output = encode(numbersSequence[0], 0, factor);
+
+  for (let i = 1; i < numbersSequence.length; i++) {
+    const a = numbersSequence[i];
+    const b = numbersSequence[i - 1];
+    output += encode(a, b, factor);;
+  }
+
+  return output;
+};
+
+export const decodeNumbersSequence = encodedNumbersSequenceStr => {
+  const numbers = [];
+  let index = 0;
+  let num = 0;
+
+  while (index < encodedNumbersSequenceStr.length) {
+    let b;
+    let shift = 0;
+    let result = 0;
+    do {
+      b = encodedNumbersSequenceStr.charAt(index++).charCodeAt(0) - 63; // finds ascii
+      // and subtract it by 63
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+
+    num += ((result & 1) !== 0 ? ~(result >> 1) : (result >> 1));
+    // shift = 0;
+    // result = 0;
+    //
+    // do {
+    //   b = encodedNumbersSequenceStr.charAt(index++).charCodeAt(0) - 63;
+    //   result |= (b & 0x1f) << shift;
+    //   shift += 5;
+    // } while (b >= 0x20);
+
+    try {
+      numbers.push(num / 1E5);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return numbers;
+};
+
+module.exports = { encodeNumbersSequence, decodeNumbersSequence };
